4a4c9bc87292b14f9cc401ef8335071426f8de94
lialic <19520208@gm.uit.edu.vn>
2021-10-19 01:15:36 +0700

    Done scaling, rotating, positioning for objects :tada:

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -2,13 +2,16 @@
 var init = () => {
     var scene = new THREE.Scene();
 
-    scene.fog = new THREE.FogExp2(0xfffffff, 0.2);
+    //scene.fog = new THREE.FogExp2(0xfffffff, 0.2);
 
     var box = getBox(1, 1, 1);
+    box.name = 'box-1'
+    
 
-    var plane = getPlane(20);
+    var plane = getPlane(7);
     //The name can be called on the parent object
     plane.name = 'plane-1';
+
 
     box.position.y = box.geometry.parameters.height/2;
     plane.rotation.x = Math.PI/2;
@@ -16,11 +19,12 @@ var init = () => {
     scene.add(box);  
     scene.add(plane);
     
+    
 
     var camera = new THREE.PerspectiveCamera(
-        45,
+        75,
         window.innerWidth/window.innerHeight,
-        1, 
+        0.1, 
         1000
     );
     camera.position.z = 5;
@@ -41,7 +45,7 @@ var init = () => {
 
 // Creating shape function
 
-function getBox(w, h, d) {
+function getBox(w, h, d, name=false) {
     var geometry = new THREE.BoxGeometry(w, h, d);
 
     var material = new THREE.MeshBasicMaterial({
@@ -52,10 +56,14 @@ function getBox(w, h, d) {
         material
     );
 
+    if( name != false) {
+        mesh.name = name
+    }
+
     return mesh;
 }
 
-function getPlane (size) {
+function getPlane (size, name=false) {
     var geometry = new THREE.PlaneGeometry(size, size);
 
     var material = new THREE.MeshBasicMaterial({
@@ -66,11 +74,14 @@ function getPlane (size) {
         geometry,
         material
     );
+    if (name != false) {
+        mesh.name = name
+    }
 
     return mesh;
 }
 
-function getHeart (x, y) {
+function getHeart (x, y, name=false) {
     const heartShape = new THREE.Shape();
     heartShape.moveTo( x + 5, y + 5 );
 
@@ -85,23 +96,57 @@ function getHeart (x, y) {
     var material = new THREE.MeshBasicMaterial({ 
         color: 0x00ff00 
     });
+
+    if (name != false) {
+        mesh.name = name
+    }
     var mesh = new THREE.Mesh( geometry, material ) ;
 }
 
-function rotating(object, x = 0, y = 0, z = 0) {
-    if (x != 0) {
-        object.rotate.x += x;
-    }
-    if (y != 0) {
-        object.rotate.y += y;
-    }
-    if(x != 0) {
-        object.rotate.z += z;
-    }
-    return (x, y, z);
+function rotate(name, parent, x = 0, y = 0, z = 0) {
+    /**
+     * Rotating function
+     * Args:
+     * name: name of the object we want to rotate
+     * parent: parent object of the one we pick
+     * x, y, z: the degree we want to change
+     */
+
+    var object = parent.getObjectByName(name);
+
+    object.rotation.x += x;
+    object.rotation.y += y;
+    object.rotation.z += z;
 }
 
+function scale(name, parent, x = 1, y = 1, z = 1){
 
+    /**
+     * Scaling function
+     * Args:
+     * name: name of the object we want to scale
+     * parent: parent object of the one we pick
+     * x, y, z: the size we want to change
+     */
+    var object = parent.getObjectByName(name);
+
+    object = object.scale.set(x, y, z);
+    
+}
+
+function position(name, parent, x =0 , y =0 , z =0){
+
+    /**
+     * Translate function
+     * Args:
+     * name: name of the object we want to move 
+     * parent: parent object of the one we pick
+     * x, y, z: the position we want to go to
+     */
+    var object = parent.getObjectByName(name);
+
+    object = object.position.set(x, y, z);
+}
 
 // Update function for continuosly rendering
 
@@ -111,9 +156,10 @@ function update(renderer, scene, camera) {
         scene,
         camera
     );
-    var plane = scene.getObjectByName('plane-1');
 
-    
+    rotate('box-1', scene, x=0.01, z = 0.02);
+    scale('box-1', scene, 0.5, 0.2, 1)
+    position('box-1', scene, 0, 2, 3)
 
     requestAnimationFrame(function() {
 
